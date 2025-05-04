4053d7676ea639cfde8799279753cb260e388b3f
quareko <enes.kulatay@gmail.com>
2025-05-04 17:32:54 +0300

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -1,14 +1,32 @@
 <?php
+// M3U8 linki dışarıdan GET parametresi olarak alınır
+if (!isset($_GET['url'])) {
+    http_response_code(400);
+    echo "URL eksik.";
+    exit;
+}
+
+$url = urldecode($_GET['url']);
+
+// Header’lar
 header("Content-Type: application/vnd.apple.mpegurl");
 header("Access-Control-Allow-Origin: *");
 
-$url = "https://hlsfreelinks.yebabayeeeee.workers.dev/https://fener2.demoscript.buzz/hls/bein-sports-1.m3u8";
-
+// Gerekirse özel HTTP header’ları (Referer, User-Agent vs.)
 $options = [
     "http" => [
-        "header" => "Referer: https://www.netsportv204.top\r\n"
+        "header" => "Referer: https://www.netsportv204.top\r\nUser-Agent: Mozilla/5.0\r\n"
     ]
 ];
 
 $context = stream_context_create($options);
-echo file_get_contents($url, false, $context);
+
+// Yayını çekip ilet
+$stream = @file_get_contents($url, false, $context);
+if ($stream === false) {
+    http_response_code(502);
+    echo "Yayın alınamadı.";
+    exit;
+}
+
+echo $stream;
